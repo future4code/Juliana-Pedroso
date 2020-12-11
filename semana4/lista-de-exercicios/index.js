a5c714ed580b262609a2b43d1490045ca25c4eda
julianapedroso <juliana.cristina@hotmail.com.br>
2020-12-11 10:45:27 -0300

    exercício 1 - interpretação de código

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,11 @@
+/* -------- LISTA DE EXERCÍCIOS -------- */
+// Exercícios de interpretação de código
+
+// EXERCÍCIO 1.
+
+// >> a função conversorDeMoeda pede ao usuário através de um prompt que digite o valor da cotação do Dólar
+// >> retorna o valor digitado multiplicado pelo valor a ser declarado em cotacao
+// >> a const meuDinheiro armazena a função conversorDeMoeda multiplicada declaração da cotacao, que é de 100
+// >> por fim, o console.log imprime o resultado da const meuDinheiro já com o valor calculado
+
+

7c8361e38c328d897eb0127629093919f2893044
julianapedroso <juliana.cristina@hotmail.com.br>
2020-12-11 14:19:28 -0300

    exercício 2 - lógica

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -79,3 +79,20 @@
 
 // console.log("Números pares: ", numerosPares)
 // console.log("Números ímpares: ", numerosImpares)
+
+// EXERCÍCIO 2.
+
+// a. booleano1 && booleano2 && !booleano4
+// >> false
+
+// b. (booleano1 && booleano2) || !booleano3
+// >> false
+
+// c. (booleano2 || booleano3) && (booleano4 || booleano1)
+// >> true
+
+// d. !(booleano2 && booleano3) || !(booleano1 && booleano3)
+// >> false
+
+// e. !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
+// >> true

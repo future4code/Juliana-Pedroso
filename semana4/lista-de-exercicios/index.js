f7b6d2c8497b9a6996f2f36de38882e2a1e6c56c
julianapedroso <juliana.cristina@hotmail.com.br>
2020-12-11 11:05:28 -0300

    exercício 2 - interpretação de código

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -8,4 +8,15 @@
 // >> a const meuDinheiro armazena a função conversorDeMoeda multiplicada declaração da cotacao, que é de 100
 // >> por fim, o console.log imprime o resultado da const meuDinheiro já com o valor calculado
 
+// EXERCÍCIO 2.
 
+// >> a função investeDinheiro recebe 2 parâmetros: tipoDeInvestimento e valor
+// >> a variável let valorAposInvestimento armazena o novo valor
+// >> a condicinal switch case tipoDeInvestimento vai receber condições diferentes para cada tipo de investimento, exemplo: Poupança, Renda Fixa, CDB
+// >> e caso a condição seja igual a variável tipoDeInvestimento, ela executa o que está dentro do bloco, que no caso, é o valor multiplicado por uma taxa correspondente ao tipo de investimento
+// >> existe um valor para default caso o usuário digite de forma incorreta
+// >> ao final, o return retorna o valor final, o valorAposInvestimento
+// >> a const novoMontante armezena a função investeDinheiro e no caso de "Ações", multiplica o valor por 150
+// >> a const segundoMontante armazena a função investeDinheiro e no caso do "Tesouro Direto", multiplica o valor por 200
+// >> por fim, o console.log imprime o resultado do novoMontante e do segundoMontante, respectivamente
+// >> como não existe um case para "Tesouro Direto", o console do segundoMontante vai ser impresso como undefined e alert vai ser acionado

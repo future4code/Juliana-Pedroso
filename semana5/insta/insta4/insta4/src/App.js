6a090debd79201dfeaba0a2923416d6fca5a966c
julianapedroso <juliana.cristina@hotmail.com.br>
2020-12-17 16:32:12 -0300

    tentativa de execução da atividade de arrays

--- semana5/insta/insta4/insta4/src/App.js
+++ semana5/insta/insta4/insta4/src/App.js
@@ -7,10 +7,77 @@ import ProfilePerfilJuliana from './img/juliana-cristina.jpg';
 import ProfilePerfilLivia from './img/livia-gomes.jpg';
 
 class App extends React.Component {
+  state = {
+    listaDePosts: [
+      {
+      nomeUsuario: 'labenu_',
+      fotoUsuario: 'https://picsum.photos/200/300?a=1',
+      fotoPost: 'https://picsum.photos/200/300?a=2',
+      }, 
+      {
+      nomeUsuario: 'juuhcristina_',
+      fotoUsuario: 'https://picsum.photos/200/300?a=3',
+      fotoPost: 'https://picsum.photos/200/300?a=4',
+      },
+      {
+      nomeUsuario: '_bananinha',
+      fotoUsuario: 'https://picsum.photos/200/300?a=5',
+      fotoPost: 'https://picsum.photos/200/300?a=6',
+      }
+    ],
+    textoNovoPost: '',
+    
+  }
+
+  onChangeTextoNovoPost = (e) => {
+    this.state({textoNovoPost: e.target.value})
+  }
+
+  adicionarPost = () => {
+    console.log('ADICIONAR POST', this.state.textoNovoPost)
+  }
+
   render() {
+    const listaDePosts = this.state.listaDePosts.map((post) =>{
+      return (
+        <Post 
+          nomeUsuario={post.nomeUsuario}
+          fotoUsuario={post.fotoUsuario}
+          fotoPost={post.fotoPost}
+        />
+      )
+    })
     return (
       <div className={'app-container'}>
-        <Post
+          <div>
+            <input placeholder="Nome de usuário"
+              type="text"
+              onChange={this.onChangeTextoNovoPost}
+              value={this.state.textoNovoPost}
+            />
+            <div>
+            <input placeholder="Foto Usuário"
+              type="text"
+              onChange={this.onChangeTextoNovoPost}
+              value={this.state.textoNovoPost}
+            />
+            </div>
+            <div>
+            <input placeholder="Foto Post"
+              type="text"
+              onChange={this.onChangeTextoNovoPost}
+              value={this.state.textoNovoPost}
+            />
+            </div>
+            <button onClick={this.adicionarPost}>Adicionar Post</button>
+          </div>
+        </div>
+      );
+    }
+  }
+
+export default App;
+        {/* <Post
           nomeUsuario={'labenu_'}
           fotoUsuario={ProfilePerfilLabenu}
           fotoPost={'https://picsum.photos/200/150?a=1'}
@@ -26,10 +93,10 @@ class App extends React.Component {
           nomeUsuario={'_liviagoms'}
           fotoUsuario={ProfilePerfilLivia}
           fotoPost={'https://picsum.photos/200/150?a=3'}
-        />
-      </div>
-    );
-  }
-}
+        /> */}
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;

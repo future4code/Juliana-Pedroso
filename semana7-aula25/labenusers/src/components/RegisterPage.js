8148dafa61c6b6c0f700566708b2543c1f95cc8d
julianapedroso <juliana.cristina@hotmail.com.br>
2021-01-12 16:56:11 -0300

    ajustes

--- semana7-aula25/labenusers/src/components/RegisterPage.js
+++ semana7-aula25/labenusers/src/components/RegisterPage.js
@@ -3,7 +3,7 @@ import React from "react";
 export default class RegisterPage extends React.Component {
   render() {
     return (
-        <li>{this.props.allUsers}</li>
+        <li></li>
     )
   }
 }

1350494e8dbfdcc31dca134c9c8d8af9f75f3341
julianapedroso <juliana.cristina@hotmail.com.br>
2021-01-15 11:51:07 -0300

    criação da requisição para criar playlist

--- semana7/labefy/labefy/src/components/CreatePlaylistPage.js
+++ semana7/labefy/labefy/src/components/CreatePlaylistPage.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import { axiosConfig, baseUrl } from "./parameters";
 
 export default class CreatePlaylistPage extends React.Component {
-    state = {
-        inputPlaylist: ''
-    }
+  state = {
+    inputPlaylist: "",
+  };
 
-    handleInputPlaylist = (e) => {
-        this.setState({ inputPlaylist: e.target.value })
-    }
+  handleInputPlaylist = (e) => {
+    this.setState({ inputPlaylist: e.target.value });
+  };
 
-    render() {
-        console.log(this.state)
-        return(
-            <div>
-                <input placeholder="digite o nome da playlist"
-                onChange={this.handleInputPlaylist}
-                value={this.state.inputPlaylist}></input>
-                <button>Criar</button>
-            </div>
-        )
-    }
+  createPlaylist = () => {
+    const body = {
+      name: this.state.inputPlaylist
+    };
+
+    axios.post(baseUrl, body, axiosConfig)
+    .then((response) => {
+        alert('Yessss! Playlist criada com sucesso!')
+    })
+    .catch((error) => {
+        console.log(error)
+        alert('Ops! Não foi possível criar a sua playlist :(')
+    })
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <input
+          placeholder="Digite o nome da playlist"
+          onChange={this.handleInputPlaylist}
+          value={this.state.inputPlaylist}
+        ></input>
+        <button onClick={this.createPlaylist}>Criar</button>
+      </div>
+    );
+  }
 }

c0dafb1f88112fe4f5fddae44e3a0cb684298ba4
julianapedroso <juliana.cristina@hotmail.com.br>
2021-01-15 14:21:26 -0300

    criada a lógica de requisição para excluir playlist e mais o confirm

--- semana7/labefy/labefy/src/components/ListPlaylistsPage.js
+++ semana7/labefy/labefy/src/components/ListPlaylistsPage.js
@@ -1,11 +1,82 @@
-import React from 'react';
+import axios from "axios";
+import React from "react";
+import { baseUrl, axiosConfig } from "./parameters";
 
 export default class ListPlaylistsPage extends React.Component {
-    render() {
-        return(
+  state = {
+    playlists: [],
+    // artists: []
+  };
+
+  componentDidMount = () => {
+    this.getAllPlaylists();
+  };
+
+  getAllPlaylists = () => {
+    axios
+      .get(baseUrl, axiosConfig)
+      .then((response) => {
+        this.setState({ playlists: response.data.result.list });
+        console.log(response.data.result.tracks);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  deletePlaylist = (id) => {
+    axios
+      .delete(`${baseUrl}/${id}`, axiosConfig)
+      .then((response) => {
+        this.getAllPlaylists();
+      })
+      .catch((error) => {});
+  };
+
+  //   getAllArtists = () => {
+  //     axios
+  //     .get(baseUrl, axiosConfig)
+  //     .then((response) => {
+  //       this.setState({ artists: response.data.result. });
+  //       console.log(response.data.result.artist)
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  //   }
+
+  render() {
+    console.log(this.state.playlists);
+    return (
+      <div>
+        <h3>Lista de Playlists: </h3>
+        {this.state.playlists.map((playlist) => {
+          return (
             <div>
-                <h3>Lista de Playlists: </h3>
+              <p>Nome: {playlist.name}</p>
+              <button
+                onClick={() => {
+                  if (
+                    window.confirm(
+                      "Tem certeza que desejar excluir esta playlist?"
+                    )
+                  ) {
+                    this.deletePlaylist(playlist.id);
+                  }
+                }}
+              >
+                x
+              </button>
             </div>
-        )
-    }
+          );
+        })}
+
+        {/* {this.state.artists.map((nameartist) => {
+            return(
+                <p>Artista: {nameartist}</p>
+            )
+        })} */}
+      </div>
+    );
+  }
 }

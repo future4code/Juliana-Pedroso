9cdf7302198b07bf44a7b11bd743f36a895d2205
julianapedroso <juliana.cristina@hotmail.com.br>
2021-01-26 19:54:40 -0300

    criada a estrutura dos componentes

---
+++ semana9/semana9-projeto/astromatch/astromatch/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './components/Home'
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Olá, mundo!</h1>
+      <Home />
+    </div>
+  );
+}
+
---
+++ semana9/semana9-projeto/astromatch/astromatch/src/components/Home.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+
+export default function Home() {
+    const []
+
+  return (
+    <div>
+    <p>foto</p>
+    <p>descrição</p>
+    <button>x</button>
+    <button>v</button>
+
+    <div>
+        <button>Tela de Matches</button>
+    </div>
+    
+    </div>
+  );
+}
+

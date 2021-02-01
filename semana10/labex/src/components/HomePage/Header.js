cca65c54caf4201440bc18b85d24afaca39fef81
julianapedroso <juliana.cristina@hotmail.com.br>
2021-02-01 17:49:43 -0300

    início da estilização

--- semana10/labex/src/components/HomePage/Header.js
+++ semana10/labex/src/components/HomePage/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Cover from '../../img/astronaut.jpg'
 
 export default function Header() {
   return (
     <div>
-      <p>Logo</p>
+      {/* <img src ={Cover} alt="Capa"/> */}
       <button>Sign in</button>
       <button>Sign up</button>
       <div>
---
+++ semana10/labex/src/components/HomePage/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+// export const HeaderCover = styled.img`
+//     background: no-repeat;
+//     background-size: cover;
+//     width: 100vw;
+//     height: 30vh;
+// `;

f29b90e2411c5ba9b7db8884bf0453da4b9aa2bd
julianapedroso <juliana.cristina@hotmail.com.br>
2021-02-03 15:47:27 -0300

    lógica para tornar a página de detalhes das viagens privadas funcionando

--- semana10/labex/src/components/TripDetailsPage/TripDetailsPage.js
+++ semana10/labex/src/components/TripDetailsPage/TripDetailsPage.js
@@ -1,11 +1,46 @@
-import { React, useState, useEffect } from 'react';
-import axios from 'axios'
-import { ContainerText } from './styled'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { baseUrl, user } from "../parameters";
+import { ContainerText, CardTrips } from "./styled";
+import { useHistory } from "react-router-dom";
 
-export default function TripDetailsPage () {
-    return(
-        <ContainerText>
-            <h1>Página com os detalhes das viagens</h1>
-        </ContainerText>
-    )
+export default function TripDetailsPage() {
+  const [tripDetail, setTripDetail] = useState({});
+  const history = useHistory()
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if (token === null) {
+        history.push('/login')
+    }
+
+    getTripDetail();
+  }, []);
+
+  const getTripDetail = () => {
+    axios
+      .get(`${baseUrl}/${user}/trip/BOs3axCrgBaohRGx7Nuw`, {
+        headers: {
+          auth: localStorage.getItem("token"),
+        },
+      })
+      .then((res) => {
+        setTripDetail(res.data.trip);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <ContainerText>
+      <h1>Página com os detalhes das viagens</h1>
+      <CardTrips>
+        <h2>{tripDetail.name}</h2>
+        <p>Planeta: {tripDetail.planet}</p>
+        <p>Descrição: {tripDetail.description}</p>
+      </CardTrips>
+    </ContainerText>
+  );
 }
--- semana10/labex/src/components/TripDetailsPage/styled.js
+++ semana10/labex/src/components/TripDetailsPage/styled.js
@@ -5,4 +5,19 @@ export const ContainerText = styled.div`
   align-items: center;
   justify-content: center;
   margin-top: 80px;
+
+`;
+
+export const CardTrips = styled.div`
+  border: 1px solid #000;
+  border-radius: 10px;
+  padding: 20px;
+
+  h2 {
+      margin: 0;
+  }
+
+   p {
+    margin: 0;
+  }
 `;

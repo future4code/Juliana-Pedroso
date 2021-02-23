e4c6dedabbe0eddd4d80c0314db2743407022bc6
julianapedroso <juliana.cristina@hotmail.com.br>
2021-02-23 11:59:55 -0300

    requisição de login funcionando agora com o Material-UI

--- semana12/labeddit/src/Pages/LoginPage.js
+++ semana12/labeddit/src/Pages/LoginPage.js
@@ -1,12 +1,45 @@
 import React, { useState } from "react";
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
-import { BASE_URL } from '../Components/parameters';
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../Components/parameters";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextField } from "@material-ui/core";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import Box from "@material-ui/core/Box";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(0.5, 0, 1),
+  },
+}));
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const history = useHistory();
+  const classes = useStyles();
 
   const onChangeEmail = (e) => {
     setEmail(e.target.value);
@@ -20,195 +53,130 @@ export default function LoginPage() {
     const body = {
       email: email,
       password: password,
-    }
+    };
 
-    axios.post(`${BASE_URL}/login`, body)
-    .then((res) => {
-      localStorage.setItem("token", res.data.token)
-      history.push("/feed")
-    })
-    .catch((err) => {
-      alert("Ops, usuário ou senha incorretos :(");
-      console.log(err)
-    })
+    axios
+      .post(`${BASE_URL}/login`, body)
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        history.push("/feed");
+      })
+      .catch((err) => {
+        alert("Ops, usuário ou senha incorretos :(");
+        console.log(err);
+      });
   };
 
-  return(
+  function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Copyright © "}
+        <Link color="inherit" href="https://material-ui.com/">
+          LabEddit
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
+
+  return (
     <div>
-      <input 
-      onChange={onChangeEmail}
-      value={email}
-      placeholder="E-mail*" 
-      type="text" 
-      required>  
-      </input>
-      
-      <input 
-      onChange={onChangePassword}
-      value={password}
-      placeholder="Password*" 
-      type="password" 
-      required>    
-      </input>
+      <Grid container style={{ minHeight: "100vh" }}>
+        <Grid item xs={12} sm={6}>
+          <img
+            src="https://www.redditstatic.com/accountmanager/bbb584033aa89e39bad69436c504c9bd.png"
+            style={{ width: "20%", height: "100%", objectFit: "cover" }}
+            alt="Capa"
+          />
+        </Grid>
+        <Grid
+          container
+          item
+          xs={12}
+          sm={6}
+          style={{ padding: 10, alignItems: "center", justifyContent: "left" }}
+        >
+          <div />
+          <div>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Grid>
+              <TextField
+                onChange={onChangeEmail}
+                value={email}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
 
-      <div>
-        <button onClick={handleLogin}>ENTRAR</button>
-        <button>CADASTRAR</button>
-      </div>
-      
+              <TextField
+                onChange={onChangePassword}
+                value={password}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              onClick={handleLogin}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Entrar
+            </Button>
 
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Cadastrar
+            </Button>
+            
+            <Grid container>
+              <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+            <Link href="#" variant="body2">
+            {"Don't have an account? Sign Up"}
+            </Link>
+            </Grid>
+            </Grid>        
+
+            <Box mt={8}>
+              <Copyright />
+            </Box>
+              
+          </div>
+        </Grid>
+      
+      </Grid>
     </div>
-  )
-
+  );
 }
-// import { useHistory } from "react-router-dom";
-// import axios from "axios";
-// import { BASE_URL } from "../Components/parameters";
-// import Avatar from "@material-ui/core/Avatar";
-// import Button from "@material-ui/core/Button";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-// import TextField from "@material-ui/core/TextField";
-// import FormControlLabel from "@material-ui/core/FormControlLabel";
-// import Checkbox from "@material-ui/core/Checkbox";
-// import Link from "@material-ui/core/Link";
-// import Grid from "@material-ui/core/Grid";
-// import Box from "@material-ui/core/Box";
-// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-// import Typography from "@material-ui/core/Typography";
-// import { makeStyles } from "@material-ui/core/styles";
-// import Container from "@material-ui/core/Container";
-
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="textSecondary" align="center">
-//       {"Copyright © "}
-//       <Link color="inherit" href="https://material-ui.com/">
-//         LabEddit
-//       </Link>{" "}
-//       {new Date().getFullYear()}
-//       {"."}
-//     </Typography>
-//   );
-// }
-
-// const useStyles = makeStyles((theme) => ({
-//   paper: {
-//     marginTop: theme.spacing(8),
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.secondary.main,
-//   },
-//   form: {
-//     width: "100%", // Fix IE 11 issue.
-//     marginTop: theme.spacing(1),
-//   },
-//   submit: {
-//     margin: theme.spacing(0.5, 0, 1),
-//   },
-// }));
-
-// export default function SignIn() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const history = useHistory();
-
-//   const onChangeEmail = (e) => {
-//     setEmail(e.target.value);
-//     console.log(e.target.value);
-//   };
-
-//   const onChangePassword = (e) => {
-//     setPassword(e.target.value);
-//   };
-
-//   const handleLogin = () => {
-//     console.log("clicou!");
-//   };
-
-//   const classes = useStyles();
-
-//   return (
-//     <Container component="main" maxWidth="xs">
-//       <CssBaseline />
-//       <div className={classes.paper}>
-//         <Avatar className={classes.avatar}>
-//           <LockOutlinedIcon />
-//         </Avatar>
-//         <Typography component="h1" variant="h5">
-//           Sign in
-//         </Typography>
-//         <form className={classes.form} noValidate>
-//           <TextField
-//             onChange={onChangeEmail}
-//             value={email}
-//             variant="outlined"
-//             margin="normal"
-//             required
-//             fullWidth
-//             id="email"
-//             label="Email Address"
-//             name="email"
-//             autoComplete="email"
-//             autoFocus
-//           />
-//           <TextField
-//             onChange={onChangePassword}
-//             value={password}
-//             variant="outlined"
-//             margin="normal"
-//             required
-//             fullWidth
-//             name="password"
-//             label="Password"
-//             type="password"
-//             id="password"
-//             autoComplete="current-password"
-//           />
-//           <FormControlLabel
-//             control={<Checkbox value="remember" color="primary" />}
-//             label="Remember me"
-//           />
-//           <Button
-//             onClick={handleLogin}
-//             type="submit"
-//             fullWidth
-//             variant="contained"
-//             color="primary"
-//             className={classes.submit}
-//           >
-//             Entrar
-//           </Button>
-
-//           <Button
-//             type="submit"
-//             fullWidth
-//             variant="contained"
-//             color="primary"
-//             className={classes.submit}
-//           >
-//             Cadastrar
-//           </Button>
-//           <Grid container>
-//             <Grid item xs>
-//               <Link href="#" variant="body2">
-//                 Forgot password?
-//               </Link>
-//             </Grid>
-//             <Grid item>
-//               <Link href="#" variant="body2">
-//                 {"Don't have an account? Sign Up"}
-//               </Link>
-//             </Grid>
-//           </Grid>
-//         </form>
-//       </div>
-//       <Box mt={8}>
-//         <Copyright />
-//       </Box>
-//     </Container>
-//   );
-// }

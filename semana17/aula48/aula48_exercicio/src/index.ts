2b27209389c3f74eba50e843e223d7313397c82f
julianapedroso <juliana.cristina@hotmail.com.br>
2021-03-29 13:44:10 -0300

    estruturação do projeto

---
+++ semana17/aula48/aula48_exercicio/src/index.ts
@@ -0,0 +1,85 @@
+import express, { Express, Request, Response } from "express";
+import knex from "knex";
+import cors from "cors";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+import { user } from "./types/user";
+
+dotenv.config();
+
+export const connection = knex({
+   client: "mysql",
+   connection: {
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+   }
+})
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors())
+
+app.get("/users/all", async function (
+   req: Request,
+   res: Response
+): Promise<void> {
+   try {
+      const users: user[] = await connection.raw(`
+         SELECT id, name, email, type
+         FROM aula48_exercicio
+      `)
+
+      if(!users.length){
+         res.statusCode = 404
+         throw new Error("User not found")
+      }
+
+      res.status(200).send(users)
+      
+   } catch (error) {
+      console.log(error)
+      res.send(error.message || error.sqlMessage)
+   }
+});
+
+app.get("/recipes/search", async (req: Request, res: Response) => {
+
+   try {
+
+      const title = req.query.title;
+
+      if (!title) {
+         throw new Error("Please send a valid title");
+      }
+
+      const result = await connection.raw(`
+      SELECT title, name AS "author", description
+         FROM recipes_aula48
+         JOIN users_aula48
+         ON user_id = users_aula48.id
+         WHERE recipes_aula48.title like "%${title}%"
+      `);
+
+      if (!result[0].length) {
+         throw new Error("No recipes found")
+      }
+
+      res.status(200).send(result[0]);
+      
+   } catch (error) {
+      res.status(400).send({message: error.message})
+      
+   }
+})
+
+const server = app.listen(process.env.PORT || 3003, () => {
+   if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Server is running in http://localhost:${address.port}`);
+   } else {
+      console.error(`Failure upon starting server.`);
+   }
+});
